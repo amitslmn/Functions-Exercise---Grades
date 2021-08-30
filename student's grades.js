f877457f37b0b67503a764d3118732c1cd0bca9d
amitslmn <amitslmn@gmail.com>
2021-08-30 21:16:32 +0300

    calc the median grade of the students grades

--- student's grades.js
+++ student's grades.js
@@ -5,12 +5,14 @@ let lowestGrade ;
 let highestGrade ;
 let len = grades.length -1 ;
 let reange ;
+let median ;
 tmpGrade.sort(function(a,b){ //built-in function for execption in sort method.(100)
     return a -b ;
 });
 lowestGrade = tmpGrade[0];
 highestGrade = tmpGrade[len];
 range = highestGrade - lowestGrade ;
-
+// calc the median grade.
+median = tmpGrade[Math.floor(len/2)];
 
 
