07205ff7e63b0b32abbac4724441fddb5f4f43cf
amitslmn <amitslmn@gmail.com>
2021-08-30 21:27:30 +0300

    fixing the code > adding functions.

--- student's grades.js
+++ student's grades.js
@@ -1,18 +1,28 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92] ;
 // First, lets find the range of the grades (difference between highest and lowest).
 let tmpGrade = grades ;
-let lowestGrade ;
-let highestGrade ;
 let len = grades.length -1 ;
-let reange ;
-let median ;
+
 tmpGrade.sort(function(a,b){ //built-in function for execption in sort method.(100)
     return a -b ;
 });
-lowestGrade = tmpGrade[0];
-highestGrade = tmpGrade[len];
-range = highestGrade - lowestGrade ;
-// calc the median grade.
-median = tmpGrade[Math.floor(len/2)];
+function range(grades,len){
+    let lowestGrade ;
+    let highestGrade ;
+    let reange ;
+    lowestGrade = tmpGrade[0];
+    highestGrade = tmpGrade[len];
+    range = highestGrade - lowestGrade ;
+    return "range: " + range ;
+    }
 
+// calc the median grade.
+function median(tmpGrade,len){
+    let median ;
+    median = tmpGrade[Math.floor(len/2)];
+    return "median: " + median ;
+}
+
+console.log(range(grades,len))
+console.log(median(tmpGrade,len))
 
