e6444915d801a5da39dbf94f4897e58e58ad036f
amitslmn <amitslmn@gmail.com>
2021-08-30 20:54:07 +0300

    students grade range.
    calc the difference between highest grade to lowest grad.

---
+++ student's grades.js
@@ -0,0 +1,16 @@
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92] ;
+// First, lets find the range of the grades (difference between highest and lowest).
+let tmpGrade = grades ;
+let lowestGrade ;
+let highestGrade ;
+let len = grades.length -1 ;
+let reange ;
+tmpGrade.sort(function(a,b){ //built-in function for execption in sort method.(100)
+    return a -b ;
+});
+lowestGrade = tmpGrade[0];
+highestGrade = tmpGrade[len];
+range = highestGrade - lowestGrade ;
+
+
+
